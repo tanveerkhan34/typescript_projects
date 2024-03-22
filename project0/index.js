c17dedcd321964b1fe1009c87d1fae6aebaf48f4
tanveerkhan <tanveeralikhan313@gmail.com>
2024-03-22 22:29:32 +0500

    basic_ts_project

---
+++ project0/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// let nameofindividual='zavvi';
+// console.log(nameofindividual);
+let teacher;
+teacher = "Maths tutor";
+teacher.toUpperCase()
+console.log(teacher);
---
+++ project0/index.ts
@@ -0,0 +1,7 @@
+// let nameofindividual='zavvi';
+// console.log(nameofindividual);
+
+let teacher;
+teacher="Maths tutor";
+teacher.toUpperCase()
+console.log(teacher);
